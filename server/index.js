20890cf5f6624f7634c1b21d7a6a376dc9713868
Abel Mathew <notify.abelmathew@gmail.com>
2021-07-24 17:45:59 +0000

    Setup Puppeteer

--- server/index.js
+++ server/index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const requestIP = require('request-ip');
 const { lookup } = require('geoip-lite');
+const puppeteer = require('puppeteer');
 
-
-
-app.use('/', (req, res) => {
+app.get('/', async (req, res) => {
 	const clientIP = requestIP.getClientIp(req);
-
-	res.send(lookup(clientIP));
+	const browser = await puppeteer.launch({
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
+	const page = await browser.newPage();
+	await page.setViewport({
+		width: 1080,
+		height: 1080,
+		deviceScaleFactor: 1,
+	});
+	console.log(req.query);
+	// { time: '10-12-00', date: '12-12-2021', timezone: 'UTC-0530' }
+	// { time: '10-12-00', date: '12-12-2021', timezone: 'UTC 0530' }
+	await page.goto(`https://timezone.designrknight.co/`, { waitUntil: 'networkidle0' });
+	await page.screenshot({ path: path.join(__dirname, `test.png`) });
+	await browser.close();
+	res.sendFile(path.join(__dirname, `test.png`));
 });
 
 app.listen(3000, () => {
