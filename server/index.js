a5b68afe8e077249d07375601a40891d717963e9
DesignrKnight <notify.abelmathew@gmail.com>
2021-07-25 04:26:41 +0000

    Puppeteer Works

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const app = express();
 const requestIP = require('request-ip');
 const { lookup } = require('geoip-lite');
 const puppeteer = require('puppeteer');
+const path = require('path');
+
 
 app.get('/', async (req, res) => {
 	const clientIP = requestIP.getClientIp(req);
@@ -18,7 +20,7 @@ app.get('/', async (req, res) => {
 	console.log(req.query);
 	// { time: '10-12-00', date: '12-12-2021', timezone: 'UTC-0530' }
 	// { time: '10-12-00', date: '12-12-2021', timezone: 'UTC 0530' }
-	await page.goto(`https://timezone.designrknight.co/`, { waitUntil: 'networkidle0' });
+	await page.goto(`http://139.162.166.77:3000/`, { waitUntil: 'networkidle0' });
 	await page.screenshot({ path: path.join(__dirname, `test.png`) });
 	await browser.close();
 	res.sendFile(path.join(__dirname, `test.png`));
